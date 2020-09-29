fb5d66367a42a0ee38913e19b816c1290ffb3f24
AnKiWoong <ankiwoong@gmail.com>
2020-09-29 13:45:42 +0900

    6강 모바일 사이트 키비주얼 영역의 제작

---
+++ 06/html source/gallery(16brand)/js/main.js
@@ -0,0 +1,63 @@
+$(function(){
+	var n=0;
+	var total=6;
+	$(".keyvisual li").eq(n).addClass("current");
+	$(".pagination li").eq(n).addClass("current");
+
+	function intervalMoving(){
+		if(n < (total-1)){ // 0, 1, 2, 3
+			n=n+1;
+		}else{
+			n=0;
+		}
+		$(".keyvisual li.current").removeClass("current");
+		$(".keyvisual li").eq(n).addClass("current");
+		$(".pagination li.current").removeClass("current");
+		$(".pagination li").eq(n).addClass("current");
+	}
+
+	var id=setInterval(intervalMoving, 5000);
+
+	$(".left").click(function(e){
+		e.preventDefault();
+
+		if(n > 0){ // 0, 1, 2, 3
+			n=n-1;
+			$(".keyvisual li.current").removeClass("current");
+			$(".keyvisual li").eq(n).addClass("current");
+			$(".pagination li.current").removeClass("current");
+			$(".pagination li").eq(n).addClass("current");
+		}
+	});
+	$(".right").click(function(e){
+		e.preventDefault();
+
+		if(n < (total-1)){ // 0, 1, 2, 3
+			n=n+1;
+			$(".keyvisual li.current").removeClass("current");
+			$(".keyvisual li").eq(n).addClass("current");
+			$(".pagination li.current").removeClass("current");
+			$(".pagination li").eq(n).addClass("current");
+		}
+	});
+	$(".pagination a").click(function(e){
+		e.preventDefault();
+		var index=$(this).parent().index();
+
+		if(n != index){
+			n=index;
+			$(".keyvisual li.current").removeClass("current");
+			$(".keyvisual li").eq(n).addClass("current");
+			$(".pagination li.current").removeClass("current");
+			$(".pagination li").eq(n).addClass("current");
+		}
+	});
+	$(".left, .right, .pagination a").hover(
+		function(){
+			clearInterval(id);
+		},
+		function(){
+			id=setInterval(intervalMoving, 5000);
+		}
+	);
+});
